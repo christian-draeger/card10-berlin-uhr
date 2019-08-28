1270d8b2e6dfb5d348b48e4b8ff7be255a0ccd61
Christian Dräger <christian.draeger1@gmail.com>
2019-08-28 17:45:10 +0200

    can render all segments including 5er steps for minutes with highlighting

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ HEIGHT = 80
 OFFSET = 1
 
 SEGMENT_WIDTH = 40
+SEGMENT_WIDTH_THIN = 14
 SEGMENT_HEIGHT = 20
 
 COLOR_BG = (80, 80, 80)
@@ -20,68 +21,77 @@ def render_bg(disp):
     disp.rect(0, 0, 160, 80, col=COLOR_BG, filled=True)
 
 
-def render_hour_x5(disp, pos, col):
+def render_hour_x5(disp, pos):
     disp.rect(SEGMENT_WIDTH * pos + OFFSET,
               OFFSET,
               SEGMENT_WIDTH * (pos + 1) - OFFSET,
               SEGMENT_HEIGHT - OFFSET,
-              col=col,
+              col=COLOR_RED_ACTIVE,
               filled=True)
 
 
-def render_hour_x1(disp, pos, col):
+def render_hour_x1(disp, pos):
     disp.rect(SEGMENT_WIDTH * pos + OFFSET,
               SEGMENT_HEIGHT,
               SEGMENT_WIDTH * (pos + 1) - OFFSET,
               SEGMENT_HEIGHT * 2 - OFFSET,
-              col=col,
+              col=COLOR_RED_INACTIVE,
               filled=True)
 
 
-def render_minute_x5(disp, pos, col):
-    disp.rect(SEGMENT_WIDTH * pos + OFFSET,
-              SEGMENT_HEIGHT,
-              SEGMENT_WIDTH * (pos + 1) - OFFSET,
+def render_minute_x5(disp, pos):
+    is_quarter = (pos + 1) % 3 is 0
+    color = COLOR_RED_ACTIVE if is_quarter else COLOR_YELLOW_ACTIVE
+
+    disp.rect(SEGMENT_WIDTH_THIN * pos + OFFSET + 3,
+              SEGMENT_HEIGHT * 2,
+              SEGMENT_WIDTH_THIN * (pos + 1) - OFFSET + 3,
               SEGMENT_HEIGHT * 3 - OFFSET,
-              col=col,
+              col=color,
               filled=True)
 
 
-def render_minute_x1(disp, pos, col):
+def render_minute_x1(disp, pos):
     disp.rect(SEGMENT_WIDTH * pos + OFFSET,
               SEGMENT_HEIGHT * 3,
               SEGMENT_WIDTH * (pos + 1) - OFFSET,
               SEGMENT_HEIGHT * 4 - OFFSET,
-              col=col,
+              col=COLOR_YELLOW_INACTIVE,
               filled=True)
 
 
 def render_hours_x5(disp):
     # localtime = utime.localtime()
     # hours = localtime[3]
+    # every 3rd 2px thicker
     for i in range(4):
-        render_hour_x5(disp, i, COLOR_RED_ACTIVE)
+        render_hour_x5(disp, i)
 
 
 def render_hours_x1(disp):
     # localtime = utime.localtime()
     # hours = localtime[3]
     for i in range(4):
-        render_hour_x1(disp, i, COLOR_RED_INACTIVE)
+        render_hour_x1(disp, i)
+
+
+def render_minutes_x5(disp):
+    for i in range(11):
+        render_minute_x5(disp, i)
 
 
 def render_minutes_x1(disp):
     # localtime = utime.localtime()
     # mins = localtime[4]
     for i in range(4):
-        render_minute_x1(disp, i, COLOR_RED_INACTIVE)
+        render_minute_x1(disp, i)
 
 
 def render(disp):
     render_bg(disp)
     render_hours_x5(disp)
     render_hours_x1(disp)
-
+    render_minutes_x5(disp)
     render_minutes_x1(disp)
     disp.update()
     disp.close()
