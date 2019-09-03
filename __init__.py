fdbae9ec64fe7af86ce86176a62f4b5f337588f6
Christian Dräger <christian.draeger1@gmail.com>
2019-09-03 16:27:23 +0200

    render second hints and make seconds display more decent

--- __init__.py
+++ __init__.py
@@ -5,11 +5,12 @@ import display
 
 class Colors(object):
     background = (20, 20, 20)
+    # background = (255, 255, 255)
     red_on = (255, 0, 0)
     red_off = (60, 0, 0)
     yellow_on = (255, 255, 0)
     yellow_off = (60, 60, 0)
-    seconds = (0, 255, 0)
+    seconds = (255, 128, 0)
 
 
 def render_bg(disp):
@@ -76,32 +77,43 @@ def render_minutes(disp):
 
 def render_seconds(disp):
     localtime = utime.localtime()
-    secs = localtime[5]
+    seconds = localtime[5]
+    secs = 60 if seconds is 0 else seconds
     start_x = 80
 
     if secs > 0:
         length = (secs - 0) * 8 if secs < 10 else 80
-        disp.rect(start_x, 0, length + start_x, 1, col=Colors.seconds, filled=True)
+        disp.rect(start_x, 0, length + start_x, 0, col=Colors.seconds, filled=True)
 
     if secs > 10:
         length = (secs - 10) * 8 if secs < 20 else 80
-        disp.rect(158, 0, 159, length, col=Colors.seconds, filled=True)
+        disp.rect(159, 0, 160, length, col=Colors.seconds, filled=True)
 
     if secs > 20:
         length = 160 - (secs - 20) * 8 if secs < 30 else 80
-        disp.rect(length, 78, 159, 79, col=Colors.seconds, filled=True)
+        disp.rect(length, 79, 160, 80, col=Colors.seconds, filled=True)
 
     if secs > 30:
         length = 80 - (secs - 30) * 8 if secs < 40 else 0
-        disp.rect(length, 78, 159, 79, col=Colors.seconds, filled=True)
+        disp.rect(length, 79, 160, 80, col=Colors.seconds, filled=True)
 
     if secs > 40:
         length = 80 - (secs - 40) * 8 if secs < 50 else 0
-        disp.rect(0, length, 1, 79, col=Colors.seconds, filled=True)
+        disp.rect(0, length, 0, 80, col=Colors.seconds, filled=True)
 
     if secs > 50:
         length = (secs - 50) * 8 if secs < 60 else 80
-        disp.rect(0, 0, length, 1, col=Colors.seconds, filled=True)
+        disp.rect(0, 0, length, 0, col=Colors.seconds, filled=True)
+
+
+def render_second_hints(disp):
+    for i in range(0, 161, 8):
+        disp.pixel(i, 0, col=Colors.seconds)
+        disp.pixel(i, 79, col=Colors.seconds)
+
+    for i in range(0, 81, 8):
+        disp.pixel(0, i, col=Colors.seconds)
+        disp.pixel(159, i, col=Colors.seconds)
 
 
 def render(disp):
@@ -109,6 +121,7 @@ def render(disp):
     render_hours(disp)
     render_minutes(disp)
     render_seconds(disp)
+    render_second_hints(disp)
     disp.update()
     disp.close()
 
